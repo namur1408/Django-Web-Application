c75ca1c99f2144c46b8ac99505d4f4b8b554cf20
oleg_androsiuk <a.oleg1408@gmail.com>
2025-10-14 22:34:34 +0200

    member's models and course's models update

--- mysite/courses_app/models.py
+++ mysite/courses_app/models.py
@@ -4,8 +4,8 @@ from members_app.models import Member
 class Course(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    start_date = models.DateField(null=True, blank=False)
-    end_date = models.DateField(null=True, blank=False)
+    start_date = models.DateTimeField(null=True, blank=False)
+    end_date = models.DateTimeField(null=True, blank=False)
     members = models.ManyToManyField(Member, related_name='courses')
 
     def __str__(self):
--- mysite/members_app/models.py
+++ mysite/members_app/models.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Member(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    phone = models.CharField(max_length=20, null=True)
+    address = models.CharField(max_length=255, null=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.user.username
